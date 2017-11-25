9d28a7e60b2f14017e081019482aebb38f75e1a9
Renato Perdiz Rodrigues <renato.rodrigues10@fatec.sp.gov.br>
2017-11-25 01:07:56 -0200

    Limpeza

---
+++ header.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-yellow">
+    <a class="navbar-brand" href="../index.php">Xtreme Xcercise</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Usuário</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Cadastrar</a>
+                    <a class="dropdown-item" href="#">Alterar</a>
+                    <a class="dropdown-item" href="#">Excluir</a>
+                    <a class="dropdown-item" href="#">Consultar</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Aparelho e Exercício</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Cadastrar</a>
+                    <a class="dropdown-item" href="#">Alterar</a>
+                    <a class="dropdown-item" href="#">Excluir</a>
+                    <a class="dropdown-item" href="#">Consultar</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Rotina</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Cadastrar</a>
+                    <a class="dropdown-item" href="#">Alterar</a>
+                    <a class="dropdown-item" href="#">Excluir</a>
+                    <a class="dropdown-item" href="#">Consultar</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ usuario/userinsert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link href="../css/font-awesome.min.css" rel="stylesheet">
+    <link href="../css/simple-line-icons.min" rel="stylesheet">
+    <link href="../css/style.min.css" rel="stylesheet">
+    <link href="../css/stylesheetjr.css" rel="stylesheet">
+
+</head>
+<body>
+    <?php include_once '../header.php'; ?>
+
+<!--Código aqui-->
+
+<script src="../js/libs/jquery.min.js"></script>
+<script src="../js/libs/tether.min.js"></script>
+<script src="../js/libs/bootstrap.min.js"></script>
+<script src="../js/libs/pace.min.js"></script>
+</body>
+</html>
